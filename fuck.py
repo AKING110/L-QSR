62854e75ec2c88a91e1b9ee77c761c198f38e141
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-10-21 22:16:10 +0500

    Create fuck.py

---
+++ fuck.py
@@ -0,0 +1,8 @@
+from platform import uname
+dchk=uname().machine.lower()
+if "aarch" in dchk:
+    dchk="aarch"
+    print(' \033[1;32m\nYou can fuck Qsr\033[1;37m')
+else:
+    print(" \n\033[1;31mSorry bro you can't fuck Qsr (32bit device)\033[1;31m")
+    exit()
