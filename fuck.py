ba357d036d547524b664554a16d728b694b43fc3
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-10-21 22:20:09 +0500

    Update fuck.py

--- fuck.py
+++ fuck.py
@@ -1,8 +1,9 @@
+import time
 from platform import uname
 dchk=uname().machine.lower()
 if "aarch" in dchk:
     dchk="aarch"
-    print(' \033[1;32m\nYou can fuck Qsr\033[1;37m')
+    print(' \033[1;32m\nYou can fuck Qsr\n\033[1;37m');time.sleep(1)
 else:
     print(" \n\033[1;31mSorry bro you can't fuck Qsr (32bit device)\033[1;31m")
     exit()
